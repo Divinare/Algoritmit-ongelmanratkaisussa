e6e77257aa501cc8cf61e65955aa0dae4050c5a7
Divinare <niemijoe@hotmail.com>
2015-02-14 00:39:28 +0200

    .

--- Algoritmit-ongelmanratkaisussa/src/Bittijono.java
+++ Algoritmit-ongelmanratkaisussa/src/Bittijono.java
@@ -3,41 +3,32 @@
  * @author Joe Course: Algorithms in problem solving
  */
 public class Bittijono {
-    
-    public static int beatifulLines = 0;
-    
-    public static void countUglyBitlines(int last, int secondLast, int loopCount, int n) {
-        if (n == loopCount) {
-            beatifulLines++;
-            return;
-        }
-        loopCount++;
-        //System.out.println("LoopCount: " + loopCount + "ugl: " + beatifulLines);
 
-        //System.out.println("Last: " + last + " secondLast: " + secondLast);
-        if (last == secondLast) {
-            if (last == 1) {
-                countUglyBitlines(0, last, loopCount, n);
-            } else {
-                countUglyBitlines(1, last, loopCount, n);
-            }
-        } else {
-            countUglyBitlines(0, last, loopCount, n);
-            countUglyBitlines(1, last, loopCount, n);
-        }
-    }
-    
     public static void main(String[] args) {
-        
-        IO io = new IO();
-        int n = io.nextInt();
-        
-        countUglyBitlines(0, 0, 2, n);
-        countUglyBitlines(0, 1, 2, n);
-        countUglyBitlines(1, 0, 2, n);
-        countUglyBitlines(1, 1, 2, n);
 
-        io.println(beatifulLines);
+        IO io = new IO();
+
+        int n = io.nextInt();
+
+        if (n == 1) {
+            io.println(2);
+        } else if (n == 2) {
+            io.println(4);
+        }
+        else {
+                        
+            long f1 = 6;
+            long f2 = 4;
+            for (int i = 0; i < (n-3); i++) {
+                long tempF1 = f1;
+                f1 = (long) ((f1+f2) % (Math.pow(10, 9)+7));
+                f2 = tempF1;
+            }
+            io.println(f1);
+        }
+        
+
+        io.close();
     }
-    
+
 }
--- Algoritmit-ongelmanratkaisussa/src/Viikko2/Elokuvat.java
+++ Algoritmit-ongelmanratkaisussa/src/Viikko2/Elokuvat.java
@@ -4,11 +4,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * @author Joe Course: Algorithms in problem solving
  */
